8c006e4d8f72c0b0e187bb0db28b20507ebbe06a
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-05-10 10:26:36 +0530

    8.4

--- 8_4.cpp
+++ 8_4.cpp
@@ -8,7 +8,63 @@ private:
     string name;
 
 public:
-    void getData(){}
+    Hospital() {}
+    Hospital(int a, string s)
+    {
+        rlno = a;
+        name = s;
+    }
+    void display()
+    {
+        cout << "roll no = " << rlno << endl;
+        cout << "name = " << name << endl;
+    }
+};
+
+class Ward : virtual public Hospital
+{
+private:
+    int wno;
+
+public:
+    Ward() {}
+    Ward(int w) { wno = w; }
+    void display()
+    {
+        Hospital::display();
+        cout << "Ward no = " << wno << endl;
+    }
+};
+
+class Room : virtual public Hospital
+{
+private:
+    int bedno;
+    string natureOillness;
+
+public:
+    Room() {}
+    Room(int a, string n)
+    {
+        bedno = a;
+        natureOillness = n;
+    }
+    void display()
+    {
+        cout << "Bed no = " << bedno << endl;
+        cout << "nature of illness = " << natureOillness << endl;
+    }
+};
+
+class Patient : public Ward, public Room
+{
+private:
+public:
+    void display()
+    {
+        Ward::display();
+        Room::display();
+    }
 };
 
 int main()
