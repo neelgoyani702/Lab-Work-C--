9cbd1aa61596b8473245c5e2ddac82ab1af84955
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-05-13 12:20:01 +0530

    9.3

--- 9_3.cpp
+++ 9_3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-class BC
+class BaseClass
 {
 public:
     int b;
@@ -9,7 +9,7 @@ public:
         cout << "b = " << b << endl;
     }
 };
-class DC : public BC
+class DrivedClass : public BaseClass
 {
 public:
     int d;
@@ -21,19 +21,19 @@ public:
 };
 int main()
 {
-    BC *bptr;
-    BC base;
+    BaseClass *bptr;
+    BaseClass base;
     bptr = &base;
     bptr->b = 100;
     cout << "bptr poins to base objects" << endl;
     bptr->show();
-    DC derived;
+    DrivedClass derived;
     bptr = &derived;
     bptr->b = 200;
     /*bptr->b = 300;*/ // wont work
     cout << "bptr now points to derived object" << endl;
     bptr->show();
-    DC *dptr;
+    DrivedClass *dptr;
     dptr = &derived;
     dptr->d = 300;
     cout << "Dptr is derived type pointer" << endl;
