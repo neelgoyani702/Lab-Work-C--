ff653aadb639755c97f143908cf3974ce67c1eb0
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-05-17 10:22:32 +0530

    11.2

--- 11_1.cpp
+++ 11_1.cpp
@@ -5,24 +5,37 @@ int main()
 {
     ofstream out;
     out.open("data.txt");
-
+    int p = out.tellp();
+    cout << p << endl;
+    if (!out)
+    {
+        cout << "Cannot open file!" << endl;
+        return 1;
+    }
     string strIN = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     // getline(cin, strIN);
     out << strIN;
+    p = out.tellp();
+    cout << p << endl;
 
     out.close();
 
     ifstream in("data.txt");
-
-    string strOUT, strTMP;
-    while (in.eof() == 0)
+    // string strTMP, strOUT;
+    // while (in.eof() == 0)
+    // {
+    //     in >> strTMP;
+    //     strOUT.append(" " + strTMP); // appending temp string to actual string
+    // }
+    // reverse(strOUT.begin(), strOUT.end()); // Reversing string
+    char ch;
+    while (p--)
     {
-        in >> strTMP;
-        strOUT.append(" " + strTMP); // appending temp string to actual string
+        in.seekg(p);
+        in >> ch;
+        cout << ch;
     }
-
-    reverse(strOUT.begin(), strOUT.end()); // Reversing string
-    cout << strOUT;
+    // cout << strOUT;
     in.close();
     return 0;
 }
--- 11_2.cpp
+++ 11_2.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-    
+    ofstream out("student.dat", ios::out | ios::binary);
+    if (!out)
+    {
+        cout << "Cannot open file!" << endl;
+        return 1;
+    }
+    float height[5];
+    // out.write();
+    out.close();
     return 0;
 }
