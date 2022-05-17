51dcba86e80608611dd631490d4c8b43b2dea6b6
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-05-17 09:32:54 +0530

    10.1

--- 10_1_d.cpp
+++ 10_1_d.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
     char name[20];
-    cout << "Enter first name then white space and then last name of a person : ";
+    cout << "Enter first name then white space and then last name of a person: ";
     cin >> name;
     cout << "Person Name : " << name << endl;
-    cout << "Enter first name then white space and then last name of a person : ";
+    cout << "Enter first name then white space and then last name of a person: ";
     cin.getline(name, 10);
     cout.write(name, 7);
     cout << "Again Enter first name then white space and then last name of a person: ";
