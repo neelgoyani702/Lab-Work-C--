2eb115130ddc29707840d34f9ba5b725266d5c78
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-05-17 10:58:40 +0530

    11.2

--- 11_2.cpp
+++ 11_2.cpp
@@ -3,14 +3,32 @@ using namespace std;
 
 int main()
 {
-    ofstream out("student.dat", ios::out | ios::binary);
+    ofstream out("student.dat", ios::binary);
     if (!out)
     {
         cout << "Cannot open file!" << endl;
         return 1;
     }
-    float height[5];
-    // out.write();
+    float heightIN[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Enter height: ";
+        cin >> heightIN[i];
+    }
+    out.write((char *)&heightIN, sizeof(heightIN));
     out.close();
+
+    ifstream in("student.dat", ios::binary);
+    if (!in)
+    {
+        cout << "Cannot open file!" << endl;
+        return 1;
+    }
+    float heightOUT[5];
+    in.read((char *)&heightOUT, sizeof(heightOUT));
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "height = " << heightOUT[i] << endl;
+    }
     return 0;
 }
